d02d6615b402a57a8a8640ab5ec76689b4514231
qinmingyuan <mingyuan0715@foxmail.com>
2023-08-19 22:55:40 +0800

    wx upload file

--- pages/profile/index.js
+++ pages/profile/index.js
@@ -1,6 +1,9 @@
+const defaultAvatarUrl = 'https://mmbiz.qpic.cn/mmbiz/icTdbqWNOwNRna42FI242Lcia07jQodd2FJGIYQfG0LAJGFxM4FbnQP6yfMxBgJ0F3YRqJCJ1aPAK2dQagdusBZg/0'
+const HOST = 'https://test.work.design/'
+
 Page({
   data: {
-    avatarUrl: '',
+    avatarUrl: defaultAvatarUrl,
     authToken: wx.getStorageSync('authToken'),
     programUser: wx.getStorageSync('programUser')
   },
@@ -11,6 +14,12 @@ Page({
     wx.startPullDownRefresh()
   },
   onChooseAvatar(e) {
+    wx.xx = e
     this.setData({ avatarUrl: e.detail.avatarUrl })
+    wx.uploadFile({
+      url: HOST + 'auth/board/user',
+      filePath: e.detail.avatarUrl,
+      name: 'avatar'
+    })
   }
 })
