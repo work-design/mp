5dfc479bc95aa7c6f906532553e08497c2ff2271
qinmingyuan <mingyuan0715@foxmail.com>
2024-05-13 21:23:29 +0800

    appjs

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 // app.js
+const HOST = wx.getExtConfigSync().host
+const APPID = wx.getAccountInfoSync().miniProgram.appId
 
 App({
   onLaunch(options) {
     const x = wx.getExtConfigSync()
     console.debug('ext Config:', x)
+
+    wx.request({
+      url: HOST + '/wechat/launch',
+      method: 'POST',
+      data: {
+        appid: APPID,
+        ...options
+      },
+      success: res => {}
+    })
   }
 })
