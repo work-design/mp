1193c1a129530f82bdcb987a05348e73c192eb32
qinmingyuan <mingyuan0715@foxmail.com>
2024-06-15 13:45:01 +0800

    logic for mobile login

---
+++ pages/mobile/index.js
@@ -0,0 +1,12 @@
+Page({
+
+  onload() {
+
+  },
+
+  getPhoneNumber (e) {
+    console.log(e.detail.code)  // 动态令牌
+    console.log(e.detail.errMsg) // 回调信息（成功失败都会返回）
+    console.log(e.detail.errno)  // 错误码（失败时返回）
+  }
+})
