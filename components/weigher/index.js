eb97d5eb345535ebb204309ec512ab955e817341
qinmingyuan <mingyuan0715@foxmail.com>
2024-09-06 17:46:41 +0800

    support

--- components/weigher/index.js
+++ components/weigher/index.js
@@ -1,8 +1,17 @@
 Component({
   observers: {
-    'numberA, numberB': (numberA, numberB) => {
+    'chs': (buffer) => {
 
+      const arr = Array(36).fill('0000000 g')
 
+      const result = buffer.match(/.{1,2}/g).map(i => String.fromCharCode(parseInt(i, 16)))
+      const weight = `${result.slice(1, 8).join('')}${result.slice(15, 17).join('')}`
+      arr.push(weight)
+      arr.shift()
+
+      if (weight && arr.every(el => el === weight)) {
+        this.setData({ value: weight })
+      }
 
       this.setData({
         sum: numberA + numberB
