0507204b28abcf625c410fbd48a4f520071fca75
qinmingyuan <mingyuan0715@foxmail.com>
2024-09-09 11:37:39 +0800

    use data

--- behaviors/weigher.js
+++ behaviors/weigher.js
@@ -1,18 +1,18 @@
 export const weigher = Behavior({
+  data: {
+    arr: Array(36).fill('0000000 g')
+  },
   observers: {
     'buffer': function(buffer) {
-
-      const arr = Array(36).fill('0000000 g')
-
       const result = buffer.match(/.{1,2}/g).map(i => String.fromCharCode(parseInt(i, 16)))
       const weight = `${result.slice(1, 8).join('')}${result.slice(15, 17).join('')}`
-      arr.push(weight)
-      arr.shift()
+      this.data.arr.push(weight)
+      this.data.arr.shift()
 
-      console.debug('---------in be', weight, this)
+      console.debug('---------in be', weight, this.data.arr)
 
       this.setData({ origin_value: weight })
-      if (weight && arr.every(el => el === weight)) {
+      if (weight && this.data.arr.every(el => el === weight)) {
         this.setData({ value: weight })
       }
     }
