8b29f4145078ff77811584961dc2222d0ce9c59c
qinmingyuan <mingyuan0715@foxmail.com>
2025-12-08 12:39:51 +0800

    doanlowd and rediect back

--- pages/download_image/index.js
+++ pages/download_image/index.js
@@ -1,11 +1,11 @@
 
 Page({
   onLoad(query) {
-
+    console.debug('Download Image Onload:', query)
     wx.downloadFile({
       url: decodeURIComponent(query.url),
       success: res => {
-        res.tempFilePath
+        console.debug('临时文件路径：', res.tempFilePath)
         wx.saveImageToPhotosAlbum({
           filePath: res.tempFilePath,
           success: () => {
@@ -13,6 +13,10 @@ Page({
               title: '已保存到相册',
               icon: 'success'
             })
+            wx.navigateBack()
+          },
+          fail: (res) => {
+            console.debug(res)
           }
         })
       }
