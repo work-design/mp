f54d05615f10c78e4945e56d2ea66c4a5a491660
qinmingyuan <mingyuan0715@foxmail.com>
2026-02-09 18:26:06 +0800

    suport dbug

--- pages/jump/index.js
+++ pages/jump/index.js
@@ -2,9 +2,11 @@ Page({
   onLoad(query) {
     console.debug('Share Onload:', query)
     const { appId, ...extra } = query
+    const extraArray = Object.entries(extra)
     this.setData({
       appId: appId,
       extra: extra,
+      extraArray: extraArray,
       title: '进入门店小程序并分享'
     })
   },
@@ -12,6 +14,7 @@ Page({
   openOther() {
     wx.navigateToMiniProgram({
       appId: this.data.appId,
+      envVersion: 'trial',
       path: '/pages/share/index',
       extraData: this.data.extra,
       fail: (res) => {
